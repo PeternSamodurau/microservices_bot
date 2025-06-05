4d0cb24ec238d2bfba0f04fd520d81ba52481199
user <spvrent@mail.ru>
2025-06-05 15:12:34 +0300

    lesson3

--- dispatcher/src/main/java/by/spvrent/service/UpdateProducerImpl.java
+++ dispatcher/src/main/java/by/spvrent/service/UpdateProducerImpl.java
@@ -1,14 +1,23 @@
 package by.spvrent.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Slf4j
 @Service
 public class UpdateProducerImpl implements UpdateProducer{
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void producer(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue,update);
     }
 }
